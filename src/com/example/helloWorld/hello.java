3e8144ddfab72d6324620ca50b89ba09a8a85ebb
ayan-de <deayan252@gmail.com>
2022-05-31 23:44:21 +0530

    Updated creating random number code

--- src/com/example/helloWorld/hello.java
+++ src/com/example/helloWorld/hello.java
@@ -1,16 +1,19 @@
 package com.example.helloWorld;
+import java.util.Random;
 import java.util.Scanner;
 public class hello {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Random r = new Random();
         System.out.println("Welcome to the guessing game");
         System.out.println("Your Name Please");
-        String name = sc.next();
+        String name = sc.nextLine();
         System.out.println("Hello " + name);
         System.out.println("Can we Start the game");
         System.out.println("1 = YES");
         System.out.println("2 - NO");
         int ans = sc.nextInt();
+        int timesTired = 0;
         if (ans == 1)
         {
             System.out.println("LETS START THE GAME!!");
@@ -18,16 +21,18 @@ public class hello {
             {
             System.out.println("enter a random number between 1 and 6");
             int input = sc.nextInt();
-            int result = (int) (Math.random() * (6 - 1 + 1)) + 1;
+            int x = r.nextInt(6)+1;
+            //int result = (int) (Math.random() * (6 - 1 + 1)) + 1;
 
-                if (input == result) {
-                    System.out.println("My number "+result);
+                if (input == x) {
+                    System.out.println("My number "+x);
                     System.out.println("you won");
                     System.exit(0);
                 }
                 else {
-                    System.out.println("My number "+result);
-                    System.out.println("Try again");
+                    timesTired ++;
+                    System.out.println("My number "+x);
+                    System.out.println("Try again. TRY number:"+timesTired);
                 }
             }
             System.out.println("you LOST!");
